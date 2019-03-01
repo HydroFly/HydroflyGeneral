c79237ef25ec7f35ea060facebd676ba6c586fe1
Adam B <adam.benabbou@hotmail.com>
2019-03-01 14:32:20 -0600

    Chloe's test codes

--- testarea/datarecorder.py
+++ testarea/datarecorder.py
@@ -4,6 +4,7 @@ import os
 import datetime
 import Adafruit_ADS1x15
 import curses
+import RPi.GPIO as GPIO
 
 #define variables
 adc = Adafruit_ADS1x15.ADS1115()
@@ -12,15 +13,19 @@ path= os.getcwd()+"/data/"
 filename= "data_"+str(d.month)+"_"+str(d.day)+"_"+str(d.hour)+"_"+str(d.minute)+".csv"
 GAIN = 1
 
+#set stuff for curses
 stdscr = curses.initscr()
 curses.noecho()
 curses.cbreak()
 stdscr.nodelay(1)
 
+#set up GPIO pin 40
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(40,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 
 stdscr.addstr(0,23,"getting data, presss a key to exit")
-
-
 
 #open file
 datafile = open(path+filename,"w+")
@@ -28,7 +33,7 @@ datafile = open(path+filename,"w+")
 #return data/put in file
 print('Reading ADS1x15 values into the file, press q to stop...')
 datafile.write('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |\n'.format(*range(4)))
-datafile.write('-' * 37)
+datafile.write('-' * 37+"\n")
 while True:
     values = [0]*4
     for i in range(4):
@@ -40,7 +45,8 @@ while True:
     c = stdscr.getch()
     if c != curses.ERR:
         break
-
+    if GPIO.input(40) == GPIO.HIGH:
+        break
 
 
 #close the file
