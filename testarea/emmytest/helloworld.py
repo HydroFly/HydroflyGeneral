72e7a0621a3cf0f7e96c80350bf657ab83c1ac37
Thomas Slusser <tbslusser@crimson.ua.edu>
2019-03-03 13:18:27 -0600

    Emmy's practice added

---
+++ testarea/emmytest/helloworld.py
@@ -0,0 +1,27 @@
+
+stupidguy = "Adam"
+
+print("live long and prosper")
+print("So far... no signs of intelligent life...")
+
+input("generate life by pressing enter")
+
+print("Introducing: " + stupidguy)
+print("Still... no signs of intelligent life found.")
+
+anything = input("please enter first number")
+
+something = input ("please enter second number")
+
+try: 
+    
+    intnum1 = int(anything)
+
+    intnum2 = int(something)
+
+    print(intnum1*intnum2)
+
+except: 
+    print("nice try")
+
+
