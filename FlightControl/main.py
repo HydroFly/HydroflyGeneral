f69232017651da79dd674a691775114d6e3f8ff8
Adam B <adam.benabbou@hotmail.com>
2019-03-06 13:19:21 -0600

    Working interrupt loop exit

--- FlightControl/main.py
+++ FlightControl/main.py
@@ -9,34 +9,28 @@ from time import sleep
 import maxSonarTTY
 import RPi.GPIO as GPIO
 
-
 ### Define hardware interrupt 
 #Hardware setup: Button between pin 23 and ground. 
-#only some pins work. Hardware limitation.
 pin = 23
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-
 ### Define ADC interface 
-
 adc = Adafruit_ADS1x15.ADS1115()
 gain = 2/3 # gain factor for board, 2/3 can read up to 6V, 1 can read up to 4.096V
 
 
 ### Define ultrasonic sensor interface
-
 serialPort = "/dev/ttyAMA0"
 maxRange = 5000  # change for 5m vs 10m sensor
 sleepTime = 0.01
 minMM = 9999
 maxMM = 0
 
-
-
 def interrupt_handler(channel):
     print("Interrupt exception")
-    return 0
+    global running
+    running = False
 
 GPIO.add_event_detect(pin, GPIO.RISING, callback=interrupt_handler, bouncetime=200)
 
@@ -58,8 +52,9 @@ def valToVolt(value, gain):
     return v
 
 
+running = True
 #Loop
-while True:
+while (running == True):
     sensorVal = adc.read_adc(1, gain)
     pressure = voltToPressure(valToVolt(sensorVal, gain))
     
